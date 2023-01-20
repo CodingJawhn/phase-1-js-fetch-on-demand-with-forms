7f87d5fbc0a161ea2b7dc5f73fd689ffe0869114
John Yang <john.yang8097@gmail.com>
2023-01-20 14:17:48 -0500

    commit tree 0fc835a8935f0d34381cabfda493055af2f3032f

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+  const inputForm = document.quereySelector(`form`)
+
+  inputForm.addEventListener(`submit`, (event) => {
+    event.preventDefault();
+    const input = document.querySelector(`input#searchByID`);
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+        const title = document.quereySelector('section#movieDetails h4');
+        const summary = document.quereySelector('section#movieDetails p');
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
